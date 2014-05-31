ef800056d3435d3f8b0db9ed1f34b890ce17f030
Yapo:Rubn Torres <ruben@schibsted.cl>
2014-05-31 16:15:37 -0400

    Layout for the landing page Add font icons and media queries

--- landing/js/main.js
+++ landing/js/main.js
@@ -1,1 +1,14 @@
+$(document).ready( function(){
 
+	$(window).scroll( function(){
+		var scr = $(document).scrollTop();
+		if(scr > 30){
+			$(".header").addClass("compact");
+			$(".section.desc").addClass("push-down");
+		}else{
+			$(".header").removeClass("compact");
+			$(".section.desc").removeClass("push-down");
+		}
+	});
+
+});
