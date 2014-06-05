37d1e0ebf85f2e74137f22aa935b79053326384b
Yapo:Rubn Torres <ruben@schibsted.cl>
2014-06-05 16:18:49 -0400

    Progress on layout for landing page

--- landing/js/main.js
+++ landing/js/main.js
@@ -1,3 +1,5 @@
+var submitted = false;
+
 $(document).ready( function(){
 
 	$(window).scroll( function(){
@@ -11,4 +13,40 @@ $(document).ready( function(){
 		}
 	});
 
+	$("select#entry_18730254").change(function(){
+		setCurrency();
+	});
+
+	$('.icheck').iCheck({
+		radioClass: 'iradio_minimal-blue',
+		increaseArea: '50%' // optional
+	});
+
+	$("input.from-to-chile").on("ifChecked", function(){
+		var label = $(this).data("label");
+		if(label){
+			$("#from-to-label").html(label);
+		}
+		setCurrency();
+	});
+
 });
+
+function setCurrency(){
+	if($("#group_1568613071_1").is(":checked") /*FROM CHILE is checked*/){
+		$("#currency").html("CLP");
+		$("#currency").removeClass("icon-bill");
+		$("#currency").addClass("currency");
+		return;
+	}
+	var cur = $("select#entry_18730254").find(":selected").data("currency");
+	if(cur){
+		$("#currency").html(cur.toUpperCase());
+		$("#currency").removeClass("icon-bill");
+		$("#currency").addClass("currency");
+	}else{
+		$("#currency").html("");
+		$("#currency").addClass("icon-bill");
+		$("#currency").removeClass("currency");
+	}
+}
